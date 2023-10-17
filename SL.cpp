fc1b1501fbc07be1f440ee02d46b04133d84f831
U-BSD\411748 <411748@SHS-16W2NW3.beaverton.k12.or.us>
2023-10-17 13:38:29 -0700

    setup vector

--- SL.cpp
+++ SL.cpp
@@ -4,21 +4,40 @@
 
 using namespace std;
 
-struct {
+struct Person{
   char f_name[];
   char l_name[];
   int id;
   float gpa;
 }
-char response[];
 
+void add(vector<Person*>& List) {
+  Person* Student = new Person();
+  cout << "Enter the first name of the student: " << endl;
+  cin.getline(Student-> f_name);
+  cout << "Enter the last name of the student: " << endl;
+  cin.getline(Student-> l_name)
+  cout << "Enter the student ID: " << endl;
+  cin >> (Student-> id);
+  cout << "Enter the student gpa: " << endl;
+  cin >> (Student-> gpa);
+  cin.ignore(80,'\n');
+  List.push_back(Student);
+}
+
+void print(vector<Person*>&List) {
+
+}
+  
 int main()
 {
+  char response(20); 
+  vector<Person*> List;
   while(response != "quit") {
     cout << "If you want to add a student type ADD. If you want to print the list type PRINT. If you want to Delete a student type DELETE. If you wish to quit type QUIT" << endl;
     response = tolower(cin.getline());
     if(response == "add") {
-      //add function
+      add(List);
     }
     else if(response == "print") {
       //print function
