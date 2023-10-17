aa9799a28853e1175a9ccf3b8d5762f866fa68b0
U-BSD\411748 <411748@SHS-16W2NW3.beaverton.k12.or.us>
2023-10-17 14:21:30 -0700

    Added comments

--- SL.cpp
+++ SL.cpp
@@ -1,9 +1,16 @@
+//Nick Braun
+//10/17/2023
+//Student list - Creates a list where users can quit, add, print, and delete students with their name, gpa, and id number
+
+//Adding librays 
 #include <iostream>
 #include <cstring>
 #include <vector>
 
+//Standord 
 using namespace std;
 
+//Struct design
 struct Person{
   char f_name[20];
   char l_name[20];
@@ -11,6 +18,7 @@ struct Person{
   float gpa;
 };
 
+//Function to add a user to the List
 void add(vector<Person*>& List) {
   Person* Student = new Person();
   cout << "Enter the first name of the student: " << endl;
@@ -25,12 +33,14 @@ void add(vector<Person*>& List) {
   List.push_back(Student);
 }
 
+//Function to print the list
 void print(vector<Person*>&List) {
   for(std::vector<Person*>:: iterator it = List.begin(); it != List.end(); ++it) {
     cout << (*it)-> f_name << " " << (*it)-> l_name << " " << (*it)-> id << " " << (*it)-> gpa << endl;
   }
 }
 
+//Function to Remove user from the list
 void remove(vector<Person*>&List) {
   int ID_R;
   cout << "Enter the student ID you want to remove" << endl;
@@ -43,20 +53,27 @@ void remove(vector<Person*>&List) {
     }
   }
 }
-  
+
+//Main function
 int main()
 { 
-  char response[20]; 
+  char response[20];
+  //Creating vector named list
   vector<Person*> List;
+  //User wants to continue using program
   while(strcmp(response, "QUIT") != 0) {
+    //Asking user what they want to do
     cout << "If you want to add a student type ADD. If you want to print the list type PRINT. If you want to Delete a student type DELETE. If you wish to quit type QUIT" << endl;
     cin >> response;
+    //User wants to add
     if(strcmp(response, "ADD") == 0) {
       add(List);
     }
+    //User wants to print
     else if(strcmp(response, "PRINT") == 0) {
       print(List);
     }
+    //User wants to delete
     else if(strcmp(response, "DELETE") == 0) {
       remove(List);
     }
