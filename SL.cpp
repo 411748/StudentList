391da9511fd1c811c816d388b083128296f10dc6
U-BSD\411748 <411748@SHS-16W2NW3.beaverton.k12.or.us>
2023-10-17 14:16:44 -0700

    finish

--- SL.cpp
+++ SL.cpp
@@ -5,45 +5,60 @@
 using namespace std;
 
 struct Person{
-  char f_name[];
-  char l_name[];
+  char f_name[20];
+  char l_name[20];
   int id;
   float gpa;
-}
+};
 
 void add(vector<Person*>& List) {
   Person* Student = new Person();
   cout << "Enter the first name of the student: " << endl;
-  cin.getline(Student-> f_name);
+  cin >> Student-> f_name;
   cout << "Enter the last name of the student: " << endl;
-  cin.getline(Student-> l_name)
+  cin >> Student-> l_name;
   cout << "Enter the student ID: " << endl;
-  cin >> (Student-> id);
+  cin >> Student-> id;
   cout << "Enter the student gpa: " << endl;
-  cin >> (Student-> gpa);
+  cin >> Student-> gpa;
   cin.ignore(80,'\n');
   List.push_back(Student);
 }
 
 void print(vector<Person*>&List) {
+  for(std::vector<Person*>:: iterator it = List.begin(); it != List.end(); ++it) {
+    cout << (*it)-> f_name << " " << (*it)-> l_name << " " << (*it)-> id << " " << (*it)-> gpa << endl;
+  }
+}
 
+void remove(vector<Person*>&List) {
+  int ID_R;
+  cout << "Enter the student ID you want to remove" << endl;
+  cin >> ID_R;
+  for(std::vector<Person*>:: iterator it = List.begin(); it != List.end(); ++it) {
+    if((*it)-> id == ID_R){
+	delete *it;
+	List.erase(it);
+	break;
+    }
+  }
 }
   
 int main()
-{
-  char response(20); 
+{ 
+  char response[20]; 
   vector<Person*> List;
-  while(response != "quit") {
+  while(strcmp(response, "QUIT") != 0) {
     cout << "If you want to add a student type ADD. If you want to print the list type PRINT. If you want to Delete a student type DELETE. If you wish to quit type QUIT" << endl;
-    response = tolower(cin.getline());
-    if(response == "add") {
+    cin >> response;
+    if(strcmp(response, "ADD") == 0) {
       add(List);
     }
-    else if(response == "print") {
-      //print function
+    else if(strcmp(response, "PRINT") == 0) {
+      print(List);
     }
-    else if(response == "delete") {
-      //delete function
+    else if(strcmp(response, "DELETE") == 0) {
+      remove(List);
     }
   }
 }
